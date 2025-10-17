2fe8119085d32f7c81c3815ef949c218df2515f6
isangwon803 <isangwon803@gmail.com>
2025-10-17 12:06:39 +0900

    수정

--- LibraryManagementSystem.java
+++ LibraryManagementSystem.java
@@ -4,7 +4,7 @@ import DataBase.*;
 /**
  * LibraryManagementSystem 클래스의 설명을 작성하세요.
  *
- * @author (작성자 이름)
+ * @author (이상원)
  * @version (버전 번호 또는 작성한 날짜)
  */
 public class LibraryManagementSystem
