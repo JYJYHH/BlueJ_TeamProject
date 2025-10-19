e7dae4fecb33591a09dd921e53c746575742bbcb
전채금 <chaegeujeon@gamil.com>
2025-10-19 15:00:29 +0900

    App 클래스 수정

--- App.java
+++ App.java
@@ -3,9 +3,9 @@ import DataBase.*;
 import java.util.*;
 
 /**
- * App 클래스의 설명을 작성하세요.
+ * App 클래스: main메소드 내부에서 수행되는 작업(①~⑦)을 처리하도록 구현하는 클래스.
  *
- * @author (작성자 이름)
+ * @author (2020315044 허진영, 2022320009 이상원, 2024320060 전채금)
  * @version (버전 번호 또는 작성한 날짜)
  */
 public class App
