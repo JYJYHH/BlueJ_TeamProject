ccfc258a3429b1bf44a6f6653dc75241f219419f
전채금 <chaegeujeon@gamil.com>
2025-10-19 15:20:09 +0900

    App 수정!

--- App.java
+++ App.java
@@ -2,7 +2,6 @@ import DataBase.*;
 import myClass.*;
 import java.util.*;
 import java.io.*;
-import java.lang.*;
 /**
  * App 클래스: main메소드 내부에서 수행되는 작업(①~⑦)을 처리하도록 구현하는 클래스.
  *
@@ -14,15 +13,19 @@ public class App
     public static void main(String[] args)
     {
         LibraryManagementSystem library = new LibraryManagementSystem();
+        
         LibDB<User> uDB = library.setUserDB("C:\\Temp\\UserData2025.txt");
         LibDB<Book> bDB = library.setBookDB("C:\\Temp\\BookData2025.txt");
+        
         library.borrowBook("2025320001","B02");
         library.borrowBook("2024320002","B03");
         library.borrowBook("2023320003","B04");
+        
         System.out.println("----- 이용자 목록 출력 -----");
         library.printDB(uDB);
         System.out.println("----- 책 목록 출력 -----");
         library.printDB(bDB);
+        System.out.println("----- 대출 현황 -----");
         library.printLoanList();
     }
 }
